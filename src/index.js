e42ce01b8f5c16d8f9dc75426325100a9f82e2dc
Raman Trus <radadendronroma@gmail.com>
2018-02-22 16:04:15 +0300

    completed!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arr=equation.replace(/\s/g,'').match(/[+-]+\d+|\d+/g);
+  let D=Math.sqrt(arr[2]*arr[2]-4*arr[0]*arr[3])
+  return [Math.round((arr[2]*(-1)+D)/(2*arr[0])),Math.round((arr[2]*(-1)-D)/(2*arr[0]))].sort((a,b)=>{return a-b})
 }
